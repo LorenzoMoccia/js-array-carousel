63d4469c191be4b6eb4ab53aa47c1b53b58f557a
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-27 10:12:22 +0200

    Inserite immagini

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+//Array immagini
+let imagesArray = [
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg',
+];
+
+//Seleziono container img
+const items = document.querySelector(".items");
+//Ciclo array img
+for(let i = 0; i < imagesArray.length; i++)
+{
+    const srcImg = imagesArray[i];
+    console.log(srcImg);
+
+    const item = `
+    <div class="item">
+        <img src="${srcImg}" alt="">
+    </div>
+    `;
+
+    items.innerHTML = items.innerHTML + item;
+}
